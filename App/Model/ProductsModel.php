e2bb4386d63ff8926512090a84cdeb795bb78c56
pierre-malardier <pierre.malardier@laplateforme.io>
2021-03-25 16:49:06 +0100

    panier ajout produit et suppression panier

--- App/Model/ProductsModel.php
+++ App/Model/ProductsModel.php
@@ -4,22 +4,18 @@ namespace App\Model;
 
 use App\Model\Model;
 
-    
-    class ProductsModel extends Model
+
+class ProductsModel extends Model
+{
+    public function byCategorie($id)
     {
-        public function byCategorie($id)
-        {
-            $product = $this->listBy("products_categorie = '$id'");
-            return $product;
-        }
-
-        public function byId($id)
-        {
-            $product = $this->listBy("products_id = '$id'");
-            return $product;
-
-        }
+        $product = $this->listBy("products_categorie = '$id'");
+        return $product;
     }
 
-
-?>
+    public function byId($id)
+    {
+        $product = $this->listBy("products_id = '$id'");
+        return $product;
+    }
+}
